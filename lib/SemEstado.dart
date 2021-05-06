40d139f4dc6d16b4e10b06402025187c1b405118
Marcos Sobral <vini15oficial@gmail.com>
2021-05-06 02:03:00 -0300

    a luz nao liga aula 7

--- lib/SemEstado.dart
+++ lib/SemEstado.dart
@@ -1,6 +1,7 @@
 
+import 'package:app01/tema.dart';
 import 'package:flutter/material.dart';
-
+import 'tema.dart';
 import 'ComEstado.dart';
 
 class AppSemEstado extends StatelessWidget{
@@ -10,13 +11,19 @@ class AppSemEstado extends StatelessWidget{
   const AppSemEstado({Key key, this.msg}) : super(key: key); 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ComEstado(),
-      theme: ThemeData(
-        primaryColor: Colors.red,
-
-      ),
+    return AnimatedBuilder(
+      animation: Tema.instance,
+       builder: (context, child)
+    {
+        return MaterialApp(
+        home: ComEstado(),
+        theme: ThemeData(
+         brightness: Tema.instance.isThemeDark
+         ? Brightness.dark
+         : Brightness.light,
+       ),
       );
+    });
     }
       
   }
